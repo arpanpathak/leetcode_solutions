d0aafb1d7ce8466d547b1789ad90b1aa46f009d4
arpanpathak <arpanpathak1996@gmail.com>
2020-07-31 02:42:49 +0530

    maximum width of binary tree

---
+++ maximum-width-of-binary-tree/Solution.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ * 
+ * { @link https://leetcode.com/problems/maximum-width-of-binary-tree }
+ * 
+ * @author Arpan Pathak
+ */
+class Solution {
+
+    class Node {
+        TreeNode node;
+        int index;
+        public Node(TreeNode node, int index) { this.node = node; this.index = index; }
+    }
+    
+    public int widthOfBinaryTree(TreeNode root) {
+        LinkedList<Node> q = new LinkedList<>();
+        int si=Integer.MAX_VALUE, ei = Integer.MIN_VALUE, maxWidth = Integer.MIN_VALUE;
+        
+        q.addLast(new Node(root, 0));
+        q.addLast(null);
+        
+        while(!q.isEmpty()) {
+            Node curr = q.pollFirst();
+            
+            // Reached end of the level
+            if (curr == null) {
+                // calculate max width by start and end indices of this level
+                maxWidth = Math.max(maxWidth, ei - si + 1);
+                
+                // reset indices 
+                ei = Integer.MIN_VALUE; si = Integer.MAX_VALUE;
+
+                // If some values are there in the tree then add end label null marker to them
+                if(!q.isEmpty())
+                    q.addLast(null);
+            } else {
+                
+                si = Math.min(si, curr.index);
+                ei = Math.max(ei, curr.index);
+                
+                // Add to queue if left child exist with index 2*curr.index  + 1
+                if(curr.node.left != null) 
+                    q.addLast(new Node(curr.node.left, 2*curr.index + 1));
+                
+                // Add to queue if right child exist with index 2*curr.index  + 2
+                if(curr.node.right != null) 
+                    q.addLast(new Node(curr.node.right, 2*curr.index + 2));
+                
+            }
+        }
+        
+        return maxWidth;
+    }
+}
